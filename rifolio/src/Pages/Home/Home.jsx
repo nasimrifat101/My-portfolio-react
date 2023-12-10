e15c0a76320f0347d58b80229b997199fc84e128
nasimrifat101 <nasimrifat101@gmail.com>
2023-12-11 00:56:43 +0600

    used data aos

--- rifolio/src/Pages/Home/Home.jsx
+++ rifolio/src/Pages/Home/Home.jsx
@@ -1,24 +1,40 @@
+import { useEffect } from "react";
 import About from "./About";
 import Banner from "./Components/Banner";
 import Info from "./Components/Info";
 import Navbar from "./Components/Navbar";
 import ContactForm from "./ContactForm";
-// import ContactMe from "./ContactForm";
 import Works from "./Works";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Banner></Banner>
-            <About></About>
-            <Works></Works>
-           <div className="hidden lg:block">
-           <Info></Info>
-           </div>
-            <ContactForm></ContactForm>
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div data-aos="fade-up" data-aos-duration="1500">
+        <Banner></Banner>
+      </div>
+      <div data-aos="fade-down" data-aos-duration="1500">
+        <About></About>
+      </div>
+      <div data-aos="fade-up" data-aos-duration="1500">
+        <Works></Works>
+      </div>
+      <div className="hidden lg:block">
+        <div data-aos="fade-down" data-aos-duration="1500">
+          <Info></Info>
         </div>
-    );
+      </div>
+      <div data-aos="fade-up" data-aos-duration="1500">
+        <ContactForm></ContactForm>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
